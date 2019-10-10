c65b2e499d5a9c7ea4cb8ad51cc35f9a66348156
Jeremy Schmidt <8888694+nocturnalBadger@users.noreply.github.com>
2019-10-09 21:02:58 -0400

    First working version of linking script

--- link.sh
+++ link.sh
@@ -1,28 +1,26 @@
 #!/bin/bash
 # Link any files present in ./userhome to their equivilant paths in ~/
 # Writes backups to ./bak with the same structure
-# set -x
 
+backupDir="$PWD/backups/$(date +%s)"
 cd userhome || (echo "userhome doesn't exist. Run from git root" && exit)
 
-backupDir=./bak-$(date +%s)
 
-find . -type f -print0 | while IFS= read -r -d '' gitfile; do
-
+find "$PWD" -type f -print0 | while IFS= read -r -d '' gitfile; do
     # Get equivilant from ~/
-    target=$(echo "$gitfile" | sed "s+^./+$HOME/+g")
-    echo "$target"
+    target=$(echo "$gitfile" | sed "s+^$PWD/+$HOME/+g")
 
     if [[ -f $target ]]; then
         mkdir -p "$backupDir"
 
         bkpName=$(echo "$target" | sed "s+^$HOME+$backupDir+g")
 
+        mkdir -p $(dirname "$bkpName")
+
         mv "$target" "$bkpName"
-        echo "Moving $target to $bkpName"
     fi
 
-    mkdir -p "$(dirname $target)"
+    mkdir -p $(dirname "$target")
 
     ln -s "$gitfile" "$target"
 done
